25e54323ef1bfa212699b64d5e1387721b9d7d49
DavidePisani <davide.pisani96@gmail.com>
2022-07-05 15:06:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <FirstAppCLI msg="La mia prima app con Vue CLI!"/>
+
+    <img alt="Vue logo" src="./assets/scimmia.jpg"> 
+  </div>
+</template>
+
+<script>
+import FirstAppCLI from './components/FirstAppCLI.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstAppCLI
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
